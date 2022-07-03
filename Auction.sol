ebefdaca8fe736fced3f153a851293f76c0241f6
hemanthbhushan <101235574+hemanthbhushan@users.noreply.github.com>
2022-07-03 22:35:45 +0530

    Update Auction.sol

--- Auction.sol
+++ Auction.sol
@@ -10,10 +10,12 @@ contract Auction{
     uint count;
     uint noofUsers;
     address public owner;
+    
     mapping(address=>uint)  userAuctionAmount;
+    mapping(address => bool) checkUser;
     uint currentBid;
     address winner;
-    uint contractBalance;
+ 
 
 
     constructor(IERC20 _token ,uint _timePeriod){
@@ -22,45 +24,67 @@ contract Auction{
         owner = msg.sender;
         timePeriod = block.timestamp+ _timePeriod;
     }
+
+
     modifier tokenCheck(IERC20 _token){
         require(_token == Zipper,"only Zipper token");
         _;
     }
+    modifier onlyOwner(address msgSender){
+         require(msgSender == owner,"only owner can announce the winner");
+         _;
+    }
+    modifier checkAmount(uint amount){
+        require(amount>100,"minimum bid should be greater than 100 ");
+        _;
+    }
+    modifier checkAuctionOpen(){
+         require(block.timestamp < timePeriod,"the auction is closed ");
+         _;
 
+    }
+    modifier checkAuctionClose(){
+         require(timePeriod < block.timestamp,"there still time for auction ");
+         _;
 
-
- 
- 
+    }
     
-function setAuction(IERC20 token , uint amount) external tokenCheck(token) {
-    require(block.timestamp < timePeriod,"the auction is closed ");
-    require(amount>100,"minimum bid should be greater than 100 ");
-    require(currentBid < amount,"amount should be greater than the previous bid");
 
-    token.safeTransferFrom(msg.sender, address(this), amount);
-   
-    userAuctionAmount[msg.sender] = amount;
+    function contractBalance(IERC20 token) public view tokenCheck(token) returns(uint){
+        return token.balanceOf(address(this));
+    }    
+    function setAuction(IERC20 token , uint amount) external tokenCheck(token) checkAmount(amount) checkAuctionOpen() {
+       
+        token.safeTransferFrom(msg.sender, address(this), amount);
     
-    currentBid = userAuctionAmount[msg.sender];
-     contractBalance = token.balanceOf(address(this));
-    winner = msg.sender;
-    }  
-    
-function retriveAutionedAmount(IERC20 token) public  tokenCheck(token) returns(bool) {
-  require(userAuctionAmount[msg.sender]<currentBid);
-  token.safeTransferFrom(address(this),msg.sender,userAuctionAmount[msg.sender]);
-  contractBalance -= userAuctionAmount[msg.sender];
-  return true;
-  }
+        userAuctionAmount[msg.sender] = amount;
+        
+        currentBid = userAuctionAmount[msg.sender];
+        
+        winner = msg.sender;
+        if(checkUser[msg.sender]==false){
+            noofUsers+=1;
+            checkUser[msg.sender]= true;
+        }
+        emit currentBidDetails(currentBid,msg.sender);
+        }  
+        
+    function retriveAutionedAmount(IERC20 token) public  tokenCheck(token) returns(bool) {
+        require(userAuctionAmount[msg.sender]<currentBid);
+        token.safeTransferFrom(address(this),msg.sender,userAuctionAmount[msg.sender]);
+        checkUser[msg.sender] = true;
+        return checkUser[msg.sender];
+    }
 
-function transferRewards(IERC20 _token) external view tokenCheck(_token)  returns(address) {
-    
-     require(timePeriod < block.timestamp,"there still time for auction ");
-    
-     require(msg.sender == owner,"only can announce the winner");
-     uint value = currentBid;
-     _token.safeTransfer(msg.sender,value);
-       return winner;
+    function transferRewards(IERC20 _token) external  tokenCheck(_token) onlyOwner(msg.sender) checkAuctionClose() returns(address) {
+        uint value = currentBid;
+        _token.safeTransfer(winner,value);
 
-   }
+        emit winnerAnnounced(winner,value);
+        return winner;
+        
+
+    }
+    event currentBidDetails(uint bidAmount, address auctionedUser);
+    event winnerAnnounced(address finalWinner,uint bid);
 }
