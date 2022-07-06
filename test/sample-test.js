2b96228caacb89649ec34628eeb2be99db68fba4
hemanth bhushan <hemanthbhushan6@gmail.com>
2022-07-06 12:07:12 +0530

    init

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,129 +1,94 @@
-// const { inputToConfig } = require("@ethereum-waffle/compiler");
-// const { getSigners } = require("@nomiclabs/hardhat-ethers/dist/src/helpers");
-// const { expect } = require("chai");
-// const { ethers, waffle } = require("hardhat");
-// const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
+const { inputToConfig } = require("@ethereum-waffle/compiler");
+const { getSigners } = require("@nomiclabs/hardhat-ethers/dist/src/helpers");
+const { expect } = require("chai");
+const { ethers, waffle } = require("hardhat");
+const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
 
-// describe("Auction", ()=>{
-//   let Auction;
-//   let auction;
-//   let owner;
-//   let signer1;
-//   let signer2;
-//   let Token;
-//   let token;
-//   beforeEach(async ()=>{
-//     [owner,signer1,signer2] = await ethers.getSigners();
-//     Token = await ethers.getContractFactory("ZipperToken",signer2.address);
-//     token= await Token.deploy();
-    
-//     Auction = await ethers.getContractFactory("Auction");
-    
-//     //Auction = await ethers.getContractFactory("Auction",owner.address); both are same
-//     auction = await Auction.deploy(token.address,720);
-//     auction.deployed();
+describe("Auction",()=>{
+    let Auction;
+    let auction;
+    let signer1;
+    let signer2;
+    let signer3;
+    let signer4;
+    let signer5;
+    let owner;
+    let RewardToken;
+    let rewardToken;
+    let BidToken;
+    let bidToken;
+    beforeEach(async ()=>{
+        [owner,signer1,signer2,signer3,signer4,signer5] = await ethers.getSigners();
 
-    
+        BidToken = await ethers.getContractFactory("ZipperToken");
+        bidToken = await BidToken.deploy();
 
-    
-//   })
-//   describe("check constructor", ()=>{
-//     it("check token",async ()=>{
-//       const auctionAddress = await auction.Zipper();
-//       expect(auctionAddress).to.equal(token.address);
+        RewardToken = await ethers.getContractFactory("RewardToken");
+        rewardToken = await RewardToken.deploy();
 
-//     });
-//     it("check owner",async ()=>{
-//       const ownerAddress = await auction.owner();
-//       expect(ownerAddress).to.equal(owner.address);
-//     })
-//     // it("checking timePeriod",async ()=>{
-//     //   const period = await auction.timePeriod();
-//     //   expect(period).to.be.greaterThan(auction.blockStamp());
+        Auction = await ethers.getContractFactory("Auction");
+        auction = await Auction.deploy();
 
-//     // })
+    })
+    describe("checking constructor",()=>{
+        it("owner check",async ()=>{
+            expect(await auction.owner()).to.equal(owner.address);
+        })
+    })
+    describe("_start Auction",()=>{
+            it("set startAuction",async ()=>{
+                await auction.connect(owner)._startAuction(bidToken.address,rewardToken.address,320);
+                expect(await auction.BidToken()).to.equal(bidToken.address);
+                expect(await auction.RewardToken()).to.equal(rewardToken.address);
+                expect(await auction.owner()).to.equal(owner.address);
+            });
+            it("calling non owner",async ()=>{
+               
+                expect(auction.connect(signer1)._startAuction(bidToken.address,rewardToken.address,320)).to.be.revertedWith("only owner can access this function");
+            })
 
+        })
+        describe("checking boredApe , cryptoPunks",()=>{
+            describe("checking boredApe with require sastified",()=>{
+                it("calling boredApe",async ()=>{
+                    await bidToken.mintToken(signer1.address,10000);
+                    await bidToken.mintToken(signer2.address,10000);
+                    await bidToken.mintToken(signer3.address,10000);
+                    await bidToken.mintToken(signer4.address,10000);
+                    await bidToken.mintToken(signer5.address,10000);
 
-//   })
-//   describe("check setAuction  function",()=>{
-//     describe("with condition sastified",()=>{
-//       it("check contract balance",async ()=>{
-//         const signerBalance = await token.balanceOf(owner.address);
-//         const contractBalance = await token.balanceOf(auction.address);
+                    await auction._startAuction(bidToken.address,rewardToken.address,150);
 
-//         // expect(signerBalance).to.equal(1000000000000000000000000);
-//         expect(contractBalance).to.equal(0);
+                    await auction.connect(owner).bidBoredApe(bidToken.address,101);
+                    await auction.connect(signer1).bidBoredApe(bidToken.address,102);
+                    await auction.connect(signer2).bidBoredApe(bidToken.address,103);
+                    await auction.connect(signer3).bidBoredApe(bidToken.address,104);
+                    await auction.connect(signer4).bidBoredApe(bidToken.address,105);
+                    await auction.connect(signer4).bidCryptoPunk(bidToken.address,105);
+                    await auction.connect(signer5).bidCryptoPunk(bidToken.address,106);
+                    await auction.connect(signer1).bidBoredApe(bidToken.address,101);;
 
-//         await auction.connect(owner).setAuction(token.address,10000);
-       
+                    
+                    const currentBidBA = await auction.currentBidBA();
+                    const currentBidCP = await auction.currentBidCP();
+                    const contractBalance = await bidToken.balanceOf(auction.address);
+                    expect(contractBalance).to.equal(827);
+                    expect(currentBidBA).to.equal(203);
+                    expect(currentBidCP).to.equal(106);
+                    expect(await auction.winnerBA()).to.equal(signer1.address);
+                    expect(await auction.winnerCP()).to.equal(signer5.address);
+                    expect(await auction.totalOnOfBidders()).to.equal(6);
 
-//         const signerBalance1 = await token.balanceOf(owner.address);
-//         const contractBalance1 = await token.balanceOf(auction.address);
-//         // expect(signerBalance1).to.equal(signerBalance-10000000);
-//         expect(contractBalance1).to.equal(10000);
-//       })
-
-//         it("checking for user auction amount",async ()=>{
-//           await auction.connect(owner).setAuction(token.address,1000);
-//           const checkUserAmount = await auction.userAuctionAmount(owner.address);
-//           expect(checkUserAmount).to.equal(1000);
-
-//         });
-//         it("current Bid",async ()=>{
-//           await auction.connect(owner).setAuction(token.address,101);
-//           await auction.connect(owner).setAuction(token.address,103);
-//           const checkCurrentBid = await auction.currentBid();
-//           expect(checkCurrentBid).to.equal(103);
-//         })
-//         it("checking winner",async ()=>{
-//           await auction.connect(owner).setAuction(token.address,101);
-          
-//           await auction.connect(owner).setAuction(token.address,102);
-          
-//           await auction.connect(owner).setAuction(token.address,1022);
-//           const checkWinner = await  auction.winner();
-//           expect(checkWinner).to.equal(owner.address);
-//         })
+                })
 
 
-//     });
+            })
+            describe("checking boredApe with require not-sastified",()=>{
+                
+            })
 
-   
-
-//     describe("with the condition not sastified by require",()=>{
-//       it("revert with only zipper  ", async ()=>{
-//         expect(auction.setAuction(signer2.address,1001)).to.be.revertedWith("only Zipper token");
+        });
         
-//       })
-//       it("revert with minimum bid should be greater than 100  ", async ()=>{
-//         expect(auction.setAuction(signer1.address,100)).to.be.revertedWith("minimum bid should be greater than 100");
-        
-//       })
-//       })
-//   });
-
-// //end of the setAuction
-// describe(" transfer rewards",()=>{
-//   it("checking transfer rewards",async ()=>{
-//     await auction.connect(owner).setAuction(token.address,1022);
-
-//     const userBalance = await token.balanceOf(owner.address);
-
-//     const checkWinner = await  auction.winner();
-//     expect(checkWinner).to.equal(owner.address);
-
-//    const checkTransferRewards= await auction.connect(owner).transferRewards(token.address);
-//   //  expect(checkTransferRewards).to.equal(checkWinner);
-
-//     expect(await token.balanceOf(owner.address)).to.equal(userBalance+1022);
-   
-
-
-//   })
-//   })
-// })
-
- 
 
 
 
@@ -131,35 +96,5 @@
 
 
 
-
-
-
-
-//   // describe("check for retrive Auction Amount",()=>{
-// //   describe("if require sastified",()=>{
-
-// //   });
-// //   describe("if required not sastified",()=>{
-// //     it("if current bit is not greater",async ()=>{
-// //       await auction.connect(owner).setAuction(token.address,101);
-// //       await auction.connect(owner).setAuction(token.address,102);
-      
-// //     })
-// //   })
-
-// // });
-
-
-
- 
-//     // await auction.connect(owner).setAuction(token.address,101);
-//     // const balanceOfUser = await token.balanceOf(owner.address);
-    
-
-//     // const checkCurrentBid = await auction.currentBid();
-//     // // expect(checkCurrentBid).to.equal(101);
-//     //  await  auction.connect(owner).transferRewards(token.address);
-
-//     // expect(checktransferRewards).to.equal(owner.address);
-//     // expect(balanceOfUser).to.equal(balanceOfUser+101);
-
+            
+        })
